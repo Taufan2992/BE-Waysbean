2bf492ffb76738ac4048bdd3f3e648866afd0801
Taufan2992 <taufan.inggil@gmail.com>
2022-09-19 12:59:58 +0700

    update

--- handlers/product.go
+++ handlers/product.go
@@ -25,7 +25,7 @@ type productHandler struct {
 }
 
 // Create `path_file` Global variable here ...
-var path_file = "https://waysbean.herokuapp.com/api/v1/uploads/"
+var path_file = "https://waysbean.herokuapp.com/api/v1/"
 
 func HandlerProduct(ProductRepository repositories.ProductRepository) *productHandler {
 	return &productHandler{ProductRepository}
